5325a669448c295595d69a7fa8d534bd92f33246
ankitadewari <ankita.dewari10@gmail.com>
2022-12-30 03:49:13 +0530

    new file

--- style.js
+++ style.js
@@ -9,6 +9,15 @@ let ticketprice = +movieselect.value;
 //function to calculate price
 function updatedselectedcount(){
     const selectedseats = document.querySelectorAll('.row.seat.selected');
+
+    //copy selected seats
+    //map thru array
+    //return new array indexes
+
+    const seatindex = [...selectedseats].map(function(seat){
+        return[...seat].indexOf(seat)
+    });
+   localStorage.setItem('selectedseats',JSON.stringify(seatindex));
     const selectedseatscount = selectedseats.length;
     count.innerText = selectedseatscount;
     total.innerText = selectedseatscount * ticketprice;
